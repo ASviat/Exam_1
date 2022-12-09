af1f0e3290f6a8f00cc4b1468e91cf1a0db8eefa
Sviatoslav <ecrprague.asi@gmail.com>
2022-12-09 13:29:37 +0300

    SomeBeautyDone

--- ExamTask/Program.cs
+++ ExamTask/Program.cs
@@ -2,8 +2,9 @@
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется
 // пользоваться коллекциями, лучше обойтись исключительно массивами.
 
-
-
+string[] words = EnterWordsToArray();
+Console.WriteLine();
+PrintWordsFromArray(words);
 
 
 string[] EnterWordsToArray()
@@ -19,20 +20,16 @@ string[] EnterWordsToArray()
     return array;
 }
 
-string[] words = EnterWordsToArray();
-
-
-
-
-PrintWordsFromArray(words);
-
 void PrintWordsFromArray(string[] array)
 {
+    Console.Write("[ ");
     for (int i = 0; i < array.Length; i++)
     {
         if (array[i].Length <= 3)
         {
-            Console.WriteLine(array[i]);
+            Console.Write(array[i]);
+            Console.Write(" ");
         }
     }
+    Console.WriteLine("]");
 }
